231ca9592ca3efb9e1cec53cf5b117b8715ee02b
Vitor Martini <vitor-martini@outlook.com>
2024-07-24 01:22:38 -0300

    feat: customized response handler

---
+++ src/utils/responseHelper.js
@@ -0,0 +1,14 @@
+function createResponse(body = null, status = 200, headers = { "Content-Type": "application/json" }) {
+  const responseOptions = {
+    status,
+    headers,
+  };
+
+  if (body !== null) {
+    responseOptions.body = JSON.stringify(body);
+  }
+
+  return new Response(body !== null ? JSON.stringify(body) : null, responseOptions);
+}
+
+module.exports = createResponse;
