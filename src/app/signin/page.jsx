067177ab98c9fb91c52420ab9575de4f24d997a8
Vitor Martini <vitor-martini@outlook.com>
2024-07-25 00:58:37 -0300

    feat: login page

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,8 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Container } from "./styles";
+import Image from "next/image";
+import { Container, WallpaperContainer, LoginContainer } from "./styles";
 import { useAuth } from "@/context/auth";
 import { useRouter } from "next/navigation";
+import hero from "@/assets/hero.png";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import Link from "next/link";
 
 const SignIn = () => {
   const router = useRouter();
@@ -21,13 +28,43 @@ const SignIn = () => {
     if(user) {
       router.push("/");
     }
-  });
+  }, [user]);
 
   return (
     <Container>
-      <input type="email" onChange={e => setEmail(e.target.value)}/>
-      <input type="password" onChange={e => setPassword(e.target.value)}/>
-      <button type="button" onClick={handleLogin}>LOGAR</button>
+      <WallpaperContainer>
+        <Image
+          src={hero}
+          alt="Hero Image"
+          layout="fill"
+          // objectFit="contain"
+          quality={100}
+        />
+      </WallpaperContainer>
+      <LoginContainer>
+        <h1>Faça Login</h1>
+        <Input 
+          icon={MdEmail}
+          placeholder="E-mail"
+          type="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <Input
+          icon={RiLockPasswordFill}
+          placeholder="Senha"
+          type="password" 
+          onChange={e => setPassword(e.target.value)}
+        />
+        
+        <Button
+          title={"Entrar"}
+          onClick={handleLogin}
+        />
+
+        <Link href="/signup">
+          Criar uma conta
+        </Link>
+      </LoginContainer>
     </Container>
   );
 };
--- src/app/signin/styles.js
+++ src/app/signin/styles.js
@@ -1,3 +1,32 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const WallpaperContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh; 
+  overflow: hidden; 
+`;
+
+export const LoginContainer = styled.div`
+  margin: 0 16rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  padding: 4rem;
+  background-color: white;
+  border-radius: 20px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.5);
+
+  > button {
+    margin-top: 2rem;
+  }
+`;
